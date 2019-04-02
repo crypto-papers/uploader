b2d2993f61275436f676c5ceb3d717c75917a793
Marek Rewers <mrewers@users.noreply.github.com>
2019-04-01 23:42:42 -0400

    Start building out form component

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Form from './Component/Form';
+
+const App = () => (
+  <Form />
+);
+
+export default App;
---
+++ src/Component/Form.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Form = () => (
+  <form>
+    <fieldset>
+
+      <label htmlFor="author">
+        Author:
+        <input
+          id="author"
+          name="author"
+          type="text"
+        />
+      </label>
+
+    </fieldset>
+  </form>
+);
+
+export default Form;
