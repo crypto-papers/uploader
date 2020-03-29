9945458547f959ef25e6efd1fc8ec515b43fcd0c
Marek Rewers <mrewers@users.noreply.github.com>
2020-03-28 23:56:31 -0400

    Adjust ESLint configs for typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,54 @@
 module.exports = {
   extends: ['@cryptopapers/eslint-config', '@cryptopapers/eslint-config/react'],
+  env: {
+    browser: true,
+    node: true,
+  },
+  ignorePatterns: ['dist/', 'node_modules/'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  overrides: [
+    {
+      extends: [
+        '@cryptopapers/eslint-config',
+        '@cryptopapers/eslint-config/react',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+      ],
+      files: ['**/*.ts', '**/*.tsx'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint'],
+      rules: {
+        'import/extensions': [
+          'error',
+          'always',
+          {
+            js: 'never',
+            jsx: 'never',
+            ts: 'never',
+            tsx: 'never',
+          },
+        ],
+        'import/no-namespace': 'off',
+        'react/jsx-filename-extension': [
+          'error',
+          {
+            extensions: ['.ts', '.tsx'],
+          },
+        ],
+        'react/prop-types': 'off',
+      },
+    },
+  ],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.json', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
 };
