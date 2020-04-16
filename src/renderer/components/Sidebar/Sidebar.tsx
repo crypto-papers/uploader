1a6ecea59ede2ceed67f82c41208fc95c7dd982e
Marek Rewers <mrewers@users.noreply.github.com>
2020-04-15 23:23:16 -0400

    Make sidebar menu items toggle-able

--- src/renderer/components/Sidebar/Sidebar.tsx
+++ src/renderer/components/Sidebar/Sidebar.tsx
@@ -5,27 +5,49 @@ import logo from '../../../static/assets/plume_color.svg';
 
 import './Sidebar.module.scss';
 
-const Sidebar: React.FC = () => (
-  <aside styleName="sidebar">
-    <img alt="" src={logo} styleName="logo" />
-    <nav styleName="nav">
-      <ul styleName="menu">
-        <li styleName="menuItem">
-          <Search size={48} styleName="icon" />
-          Search For Paper
-        </li>
-        <li styleName="menuItem active">
-          <FileText size={48} styleName="icon" />
-          Add/Edit Paper
-        </li>
-        <li styleName="menuItem">
-          <Settings size={48} styleName="icon" />
-          Settings
-        </li>
-      </ul>
-    </nav>
-  </aside>
-);
+const Sidebar: React.FC = () => {
+  const [selected, setSelected] = React.useState('search');
+
+  return (
+    <aside styleName="sidebar">
+      <img alt="" src={logo} styleName="logo" />
+      <nav styleName="nav">
+        <ul styleName="menu">
+          <li styleName="menu-li">
+            <button
+              styleName={selected === 'search' ? 'menu-item active' : 'menu-item'}
+              type="button"
+              onClick={(): void => setSelected('search')}
+            >
+              <Search size={48} styleName="icon" />
+              Search For Paper
+            </button>
+          </li>
+          <li styleName="menu-li">
+            <button
+              styleName={selected === 'file' ? 'menu-item active' : 'menu-item'}
+              type="button"
+              onClick={(): void => setSelected('file')}
+            >
+              <FileText size={48} styleName="icon" />
+              Add/Edit Paper
+            </button>
+          </li>
+          <li styleName="menu-li">
+            <button
+              styleName={selected === 'settings' ? 'menu-item active' : 'menu-item'}
+              type="button"
+              onClick={(): void => setSelected('settings')}
+            >
+              <Settings size={48} styleName="icon" />
+              Settings
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </aside>
+  );
+};
 
 Sidebar.displayName = 'Sidebar';
 
